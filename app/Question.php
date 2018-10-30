303aa4e322df75c5da326c8b4d9f4827719a409f
perlgal26 <pawarsheetal26@gmail.com>
2018-10-30 13:29:44 +1000

    added question model

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+
+    protected $fillable = ['title', 'body'];    
     //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    // it will return user data asscociated with question
+    // $question = Question::find($user_id)
+    // $question->user->name;
 }

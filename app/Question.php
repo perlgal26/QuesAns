11fcc957e6a148b6a56ee018fad9c0338a5c4285
perlgal26 <pawarsheetal26@gmail.com>
2018-10-31 15:12:25 +1000

    added data to populate database using faker

--- app/Question.php
+++ app/Question.php
@@ -1,10 +1,21 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Question extends Model
 {
-    //
+    protected $fillable = ['title', 'body'];    
+    // it will return user data asscociated with question
+    // $question = Question::find($user_id)
+    // $question->user->name;
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    
+
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
 }
